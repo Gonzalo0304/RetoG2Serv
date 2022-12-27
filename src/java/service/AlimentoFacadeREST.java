73e5987718cf99e24973c38da3662b2baf36b3f5
josue <josue@Orlando>
2022-12-27 17:11:56 +0100

    Resful, GET acabado sin comentarios

--- src/java/service/AlimentoFacadeREST.java
+++ src/java/service/AlimentoFacadeREST.java
@@ -6,6 +6,7 @@
 package service;
 
 import entities.Alimento;
+import entities.TipoAlimento;
 import excepciones.ReadException;
 import java.util.Collection;
 import java.util.logging.Level;
@@ -25,42 +26,292 @@ import javax.ws.rs.Produces;
  * @author josue
  */
 @Path("entities.alimento")
-public class AlimentoFacadeREST{
+public class AlimentoFacadeREST {
+
     @EJB
     private AlimentoInterface ejb;
 
-    
-       public AlimentoFacadeREST() {
-      
+    public AlimentoFacadeREST() {
+
     }
-    
+
     @GET
     @Produces({"application/xml"})
     public Collection<Alimento> getAlimentoTodos() {
- 
-       Collection<Alimento> alimentos = null;
+
+        Collection<Alimento> alimentos = null;
         try {
-            alimentos= ejb.getAlimentoTodos();
+            alimentos = ejb.getAlimentoTodos();
         } catch (ReadException ex) {
             Logger.getLogger(AlimentoFacadeREST.class.getName()).log(Level.SEVERE, null, ex);
         }
         return alimentos;
     }
+
+    /**
+     *
+     * @param id
+     * @return
+     */
     @GET
-    @Path("{id}")
+    @Path("AlimentoId/{id}")
     @Produces({"application/xml"})
     public Alimento getAlimentoPorId(@PathParam("id") String id) {
-                Alimento alimento = null;
-                
+        Alimento alimento = null;
+
         try {
-            alimento= ejb.getAlimentoPorId(id);
+            alimento = ejb.getAlimentoPorId(id);
         } catch (ReadException ex) {
             Logger.getLogger(AlimentoFacadeREST.class.getName()).log(Level.SEVERE, null, ex);
         }
         return alimento;
     }
 
+    /**
+     *
+     * @param nombre
+     * @return
+     */
+    @GET
+    @Path("AlimentoNombre/{nombre}")
+    @Produces({"application/xml"})
+    public Collection<Alimento> getAlimentoPorNombre(@PathParam("nombre") String nombre) {
+        Collection<Alimento> alimento = null;
+        try {
+            alimento = ejb.getAlimentoPorNombre(nombre);
+        } catch (ReadException ex) {
+            Logger.getLogger(AlimentoFacadeREST.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return alimento;
 
+    }
 
+    /**
+     *
+     * @param tipo
+     * @return
+     */
+    @GET
+    @Path("AlimentoTIPO/{tipo}")
+    @Produces({"application/xml"})
+    public Collection<Alimento> getAlimentoPorTipo(@PathParam("tipo") String tipo) {
+        Collection<Alimento> alimentos = null;
+        TipoAlimento tipoAlimento = null;
+        tipoAlimento = tipoAlimento.valueOf(tipo.toUpperCase());
+        try{
+        alimentos = ejb.getAlimentoPorTipo(tipoAlimento);
+            } catch (ReadException ex) {
+            Logger.getLogger(AlimentoFacadeREST.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return alimentos;
+
+    }
+
+    /**
+     *
+     * @param calorias
+     * @return
+     */
+    @GET
+    @Path("AlimentoCaloriasSuperior/{calorias}")
+    @Produces({"application/xml"})
+    public Collection<Alimento> getAlimentoPorCaloriasSuperior(@PathParam("calorias") Float calorias){
+        Collection<Alimento> alimentos = null;
+        try {
+            alimentos = ejb.getAlimentoPorCaloriasSuperior(calorias);
+        } catch (ReadException ex) {
+            Logger.getLogger(AlimentoFacadeREST.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return alimentos;
+
+    }
     
+       @GET
+    @Path("AlimentoCaloriasMinimo/{calorias}")
+    @Produces({"application/xml"})
+    public Collection<Alimento> getAlimentoPorCaloriasMinimo(@PathParam("calorias") Float calorias){
+        Collection<Alimento> alimentos = null;
+        try {
+            alimentos = ejb.getAlimentoPorCaloriasMinimo(calorias);
+        } catch (ReadException ex) {
+            Logger.getLogger(AlimentoFacadeREST.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return alimentos;
+
+    }
+
+
+    /**
+     *
+     * @param caloriasMax
+     * @param caloriasMin
+     * @return
+     */
+    @GET
+    @Path("AlimentoCaloriasEntre/{caloriasMax}/{caloriasMin}")
+    @Produces({"application/xml"})
+    public Collection<Alimento> getAlimentoPorCaloriasEntre(@PathParam("caloriasMax") Float caloriasMax,@PathParam("caloriasMin") Float caloriasMin ){
+        Collection<Alimento> alimentos = null;
+        try {
+            alimentos = ejb.getAlimentoPorCaloriasEntre(caloriasMax, caloriasMin);
+        } catch (ReadException ex) {
+            Logger.getLogger(AlimentoFacadeREST.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return alimentos;
+
+    }
+    
+        /**
+     *
+     * @param grasas
+     * @return
+     */
+    @GET
+    @Path("AlimentoGrasasSuperior/{grasas}")
+    @Produces({"application/xml"})
+    public Collection<Alimento> getAlimentoPorGrasasSuperior(@PathParam("grasas") Float grasas){
+        Collection<Alimento> alimentos = null;
+        try {
+            alimentos = ejb.getAlimentoPorGrasasSuperior(grasas);
+        } catch (ReadException ex) {
+            Logger.getLogger(AlimentoFacadeREST.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return alimentos;
+
+    }
+    
+       @GET
+    @Path("AlimentoGrasasMinimo/{grasas}")
+    @Produces({"application/xml"})
+    public Collection<Alimento> getAlimentoPorGrasasMinimo(@PathParam("grasas") Float grasas){
+        Collection<Alimento> alimentos = null;
+        try {
+            alimentos = ejb.getAlimentoPorGrasasMinimo(grasas);
+        } catch (ReadException ex) {
+            Logger.getLogger(AlimentoFacadeREST.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return alimentos;
+
+    }
+
+
+    /**
+     *
+     * @param grasasMax
+     * @param grasasMin
+     * @return
+     */
+    @GET
+    @Path("AlimentoGrasasEntre/{grasasMax}/{grasasMin}")
+    @Produces({"application/xml"})
+    public Collection<Alimento> getAlimentoPorGrasasEntre(@PathParam("grasasMax") Float grasasMax,@PathParam("grasasMin") Float grasasMin ){
+        Collection<Alimento> alimentos = null;
+        try {
+            alimentos = ejb.getAlimentoPorGrasasEntre(grasasMax, grasasMin);
+        } catch (ReadException ex) {
+            Logger.getLogger(AlimentoFacadeREST.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return alimentos;
+
+    }
+    
+        @GET
+    @Path("AlimentoProteinasSuperior/{proteinas}")
+    @Produces({"application/xml"})
+    public Collection<Alimento> getAlimentoPorProteinasSuperior(@PathParam("proteinas") Float proteinas){
+        Collection<Alimento> alimentos = null;
+        try {
+            alimentos = ejb.getAlimentoPorProteinasSuperior(proteinas);
+        } catch (ReadException ex) {
+            Logger.getLogger(AlimentoFacadeREST.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return alimentos;
+
+    }
+    
+       @GET
+    @Path("AlimentoProteinasMinimo/{proteinas}")
+    @Produces({"application/xml"})
+    public Collection<Alimento> getAlimentoPorProteinasMinimo(@PathParam("proteinas") Float proteinas){
+        Collection<Alimento> alimentos = null;
+        try {
+            alimentos = ejb.getAlimentoPorProteinasMinimo(proteinas);
+        } catch (ReadException ex) {
+            Logger.getLogger(AlimentoFacadeREST.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return alimentos;
+
+    }
+
+
+    /**
+     *
+     * @param proteinasMax
+     * @param proteinasMin
+     * @return
+     */
+    @GET
+    @Path("AlimentoProteinasEntre/{proteinasMax}/{proteinasMin}")
+    @Produces({"application/xml"})
+    public Collection<Alimento> getAlimentoPorProteinasEntre(@PathParam("proteinasMax") Float proteinasMax,@PathParam("proteinasMin") Float proteinasMin ){
+        Collection<Alimento> alimentos = null;
+        try {
+            alimentos = ejb.getAlimentoPorProteinasEntre(proteinasMax, proteinasMin);
+        } catch (ReadException ex) {
+            Logger.getLogger(AlimentoFacadeREST.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return alimentos;
+
+    }
+    
+            @GET
+    @Path("AlimentoCarbohidratosSuperior/{carbohidratos}")
+    @Produces({"application/xml"})
+    public Collection<Alimento> getAlimentoPorCarbohidratosSuperior(@PathParam("carbohidratos") Float carbohidratos){
+        Collection<Alimento> alimentos = null;
+        try {
+            alimentos = ejb.getAlimentoPorCarbohidratosSuperior(carbohidratos);
+        } catch (ReadException ex) {
+            Logger.getLogger(AlimentoFacadeREST.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return alimentos;
+
+    }
+    
+       @GET
+    @Path("AlimentoCarbohidratosMinimo/{carbohidratos}")
+    @Produces({"application/xml"})
+    public Collection<Alimento> getAlimentoPorCarbohidratosMinimo(@PathParam("carbohidratos") Float carbohidratos){
+        Collection<Alimento> alimentos = null;
+        try {
+            alimentos = ejb.getAlimentoPorCarbohidratosMinimo(carbohidratos);
+        } catch (ReadException ex) {
+            Logger.getLogger(AlimentoFacadeREST.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return alimentos;
+
+    }
+
+
+    /**
+     *
+     * @param proteinasMax
+     * @param proteinasMin
+     * @return
+     */
+    @GET
+    @Path("AlimentoCarbohidratosEntre/{carbohidratosMax}/{carbohidratosMin}")
+    @Produces({"application/xml"})
+    public Collection<Alimento> getAlimentoPorCarbohidratosEntre(@PathParam("carbohidratosMax") Float carbohidratosMax,@PathParam("carbohidratosMin") Float carbohidratosMin ){
+        Collection<Alimento> alimentos = null;
+        try {
+            alimentos = ejb.getAlimentoPorCarbohidratosEntre(carbohidratosMax, carbohidratosMin);
+        } catch (ReadException ex) {
+            Logger.getLogger(AlimentoFacadeREST.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return alimentos;
+
+    }
+    
+
 }
