671256b7372d4c97d60e44e45f434efe722360b6
jon <jon.novoa.alaiz@gmail.com>
2023-01-04 17:47:25 +0100

    facadeRest,ejb,cliente y receta, falta query tipo y clienteDieta

---
+++ src/java/service/ClienteInterface.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package service;
+
+import entities.Cliente;
+import entities.Dietista;
+import entities.Receta;
+import entities.Usuario;
+import excepciones.CreateException;
+import excepciones.DeleteException;
+import excepciones.ReadException;
+import excepciones.UpdateException;
+import java.util.Collection;
+
+/**
+ *
+ * @author somor
+ */
+public interface ClienteInterface {
+    
+    public Collection<Cliente> getClienteTodos()throws ReadException;
+    
+   public Cliente getClientePorId(String idCliente) throws ReadException;
+   
+   public void crearCliente(Cliente cliente)throws CreateException;
+   
+   public void modificarCliente(Cliente cliente) throws UpdateException;
+   
+   public void borrarCliente (Cliente cliente) throws DeleteException;
+    
+}
--- src/java/service/RecetaFacadeREST.java
+++ src/java/service/RecetaFacadeREST.java
@@ -5,14 +5,25 @@
  */
 package service;
 
+//import static com.sun.xml.internal.ws.spi.db.BindingContextFactory.LOGGER;
+import entities.Alimento;
 import entities.Receta;
+import excepciones.CreateException;
+import excepciones.DeleteException;
+import excepciones.ReadException;
+import excepciones.UpdateException;
+import java.util.Collection;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.InternalServerErrorException;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
@@ -24,17 +35,180 @@ import javax.ws.rs.core.MediaType;
  *
  * @author josue
  */
-@Stateless
-@Path("entities.receta")
-public class RecetaFacadeREST extends AbstractFacade<Receta> {
 
-    @PersistenceContext(unitName = "Reto2G2ServPU")
-    private EntityManager em;
+@Path("entities.receta")
+public class RecetaFacadeREST  {
+    
+     @EJB
+    private RecetaInterface ejb;
+
+     // @PersistenceContext(unitName = "Reto2G2ServPU")
+   // private EntityManager em;
 
     public RecetaFacadeREST() {
-        super(Receta.class);
+       // super(Receta.class);
     }
 
+  @GET
+    @Path("{id}")
+    @Produces({"application/xml"})
+    public Receta getRecetaPorId(@PathParam("id") String id) {
+        Receta receta = null;
+
+        try {
+            receta = ejb.getRecetaPorId(id);
+        } catch (ReadException ex) {
+            Logger.getLogger(RecetaFacadeREST.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return receta;
+    }
+    
+      @GET
+     // @Path("nombreDietistae")
+    @Produces({"application/xml"})
+    public Collection<Receta> getRecetaTodos() {
+
+        Collection<Receta> receta = null;
+        try {
+            receta = ejb.getRecetaTodos();
+        } catch (ReadException ex) {
+            Logger.getLogger(AlimentoFacadeREST.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return receta;
+    }
+    
+     
+        @GET
+        @Path("nombreDietista/ordenadoAlfabeticamente")
+    @Produces({"application/xml"})
+    public Collection<Receta> getRecetasAlfabeticamente() {
+
+        Collection<Receta> receta = null;
+        try {
+            receta = ejb.getRecetasAlfabeticamente();
+        } catch (ReadException ex) {
+            Logger.getLogger(AlimentoFacadeREST.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return receta;
+    }
+    
+    
+        
+        @GET
+        @Path("nombreDietista/ordenadoFecha")
+    @Produces({"application/xml"})
+    public Collection<Receta> getRecetaFechaCreacion() {
+
+        Collection<Receta> receta = null;
+        try {
+            receta = ejb.getRecetaFechaCreacion();
+        } catch (ReadException ex) {
+            Logger.getLogger(AlimentoFacadeREST.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return receta;
+    }
+    
+    
+    
+  
+    
+    
+    
+             @GET
+          @Path("nombreDietista/{nombre}")
+    @Produces({"application/xml"})
+    public Collection<Receta> getRecetaNombreDietista(@PathParam("nombre") String nombre) {
+
+        Collection<Receta> receta = null;
+        try {
+            receta = ejb.getRecetaNombreDietista(nombre);
+        } catch (ReadException ex) {
+            Logger.getLogger(AlimentoFacadeREST.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return receta;
+    }
+    
+               @GET
+          @Path("nombreReceta/{nombreReceta}")
+    @Produces({"application/xml"})
+    public Collection<Receta> getNombreReceta(@PathParam("nombreReceta") String nombreReceta) {
+
+        Collection<Receta> receta = null;
+        try {
+            receta = ejb.getNombreReceta(nombreReceta);
+        } catch (ReadException ex) {
+            Logger.getLogger(AlimentoFacadeREST.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return receta;
+    }
+    
+               @GET
+          @Path("tipoReceta/{tipoReceta}")
+    @Produces({"application/xml"})
+    public Collection<Receta> getRecetaTipo(@PathParam("tipoReceta") String tipoReceta) {
+
+        Collection<Receta> receta = null;
+        try {
+            receta = ejb.getRecetaTipo(tipoReceta);
+        } catch (ReadException ex) {
+            Logger.getLogger(AlimentoFacadeREST.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return receta;
+    }
+    
+   @POST
+   @Consumes({"application/xml"})
+   public void crearReceta(Receta receta){
+       try{
+           ejb.crearReceta(receta);
+       } catch (CreateException ex) {
+             Logger.getLogger(RecetaFacadeREST.class.getName()).log(Level.SEVERE, null, ex);
+         }
+   }
+    
+    @PUT
+     @Consumes({"application/xml"})
+     public void modificarReceta(Receta receta){
+       try{
+           ejb.modificarReceta(receta);
+       } catch (UpdateException ex) {
+             Logger.getLogger(RecetaFacadeREST.class.getName()).log(Level.SEVERE, null, ex);
+         }
+     }
+         @DELETE
+        // @Consumes({"application/xml"})
+    @Path("{id}")
+    public void removeAccount(@PathParam("id") String id) {
+        try {
+          
+            ejb.borrarReceta(ejb.getRecetaPorId(id));
+        } catch (ReadException|DeleteException ex) {
+           // LOGGER.severe(ex.getMessage());
+            throw new InternalServerErrorException(ex.getMessage());        
+        }
+    }        
+   
+       
+   }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    /**
     @POST
     @Override
     @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
@@ -87,5 +261,4 @@ public class RecetaFacadeREST extends AbstractFacade<Receta> {
     protected EntityManager getEntityManager() {
         return em;
     }
-    
-}
+**/
